9f2fce3fc9915bc196053378ae860c7378f5033a
tianfenghan <rango@swoole.com>
2023-12-05 10:56:22 +0800

    Add License

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,14 +1,7 @@
 <?php
 
 declare(strict_types=1);
-/**
- * This file is part of Hyperf.
- *
- * @link     https://www.hyperf.io
- * @document https://hyperf.wiki
- * @contact  group@hyperf.io
- * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
- */
+
 ini_set('display_errors', 'on');
 ini_set('display_startup_errors', 'on');
 
