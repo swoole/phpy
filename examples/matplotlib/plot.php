e69d980b86fedbe48968e302543132a8eab829db
tianfenghan <rango@swoole.com>
2023-12-11 14:58:05 +0800

    Update composer.json

--- examples/matplotlib/plot.php
+++ examples/matplotlib/plot.php
@@ -1,4 +1,8 @@
 <?php
+require dirname(__DIR__, 2) . '/vendor/autoload.php';
+$plt = python\matplotlib\pyplot::import();
 
-$os = python\os::import();
-
+$x = new PyList([1, 2, 3, 4]);
+$y = new PyList([30, 20, 50, 60]);
+$plt->plot($x, $y);
+$plt->show();
--- examples/os.php
+++ examples/os.php
@@ -1,10 +1,13 @@
 <?php
-function main() {
-    $m = PyCore::import("os");
+require dirname(__DIR__) . '/vendor/autoload.php';
+function main()
+{
+    $m = python\os::import();
     var_dump($m instanceof PyObject);
     $rs = $m->uname();
     echo $rs;
     echo $rs->version;
+    echo PHP_EOL;
 }
 
 main();
