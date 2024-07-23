809e6f2cf6b891596cfdde0e748571909af69a5d
Tianfeng.Han <rango@swoole.com>
2024-07-23 10:40:52 +0800

    Update transformers.php

--- examples/transformers.php
+++ examples/transformers.php
@@ -4,7 +4,7 @@ $AutoTokenizer = $transformers->AutoTokenizer;
 $AutoModelForSequenceClassification = $transformers->AutoModelForSequenceClassification;
 
 $os = PyCore::import('os');
-$os->environ->__setitem__('https_proxy', getenv('https_proxy'));
+$os->environ['https_proxy'] = getenv('https_proxy');
 
 $tokenizer = $AutoTokenizer->from_pretrained("lxyuan/distilbert-base-multilingual-cased-sentiments-student");
 $model = $AutoModelForSequenceClassification->from_pretrained("lxyuan/distilbert-base-multilingual-cased-sentiments-student");
