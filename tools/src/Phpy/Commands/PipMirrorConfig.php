ef4aa362cd3e2cf4d62add318cd53b05463ed750
chaz6chez <chaz6chez1993@outlook.com>
2025-04-01 16:33:06 +0800

    1. optimized config:pip-mirror command

--- tools/src/Phpy/Commands/PipMirrorConfig.php
+++ tools/src/Phpy/Commands/PipMirrorConfig.php
@@ -4,11 +4,42 @@ declare(strict_types=1);
 
 namespace PhpyTool\Phpy\Commands;
 
-use Symfony\Component\Console\Helper\QuestionHelper;
 use Symfony\Component\Console\Question\ChoiceQuestion;
 
 class PipMirrorConfig extends AbstractCommand
 {
+
+    /**
+     * 预设的 pip 镜像源
+     *
+     * @var array|string[]
+     */
+    protected static array $mirrors = [
+        'Python官方' => 'https://pypi.org/simple/',
+        '清华' => 'https://pypi.tuna.tsinghua.edu.cn/simple',
+        '阿里云' => 'http://mirrors.aliyun.com/pypi/simple/',
+        '华为云' => 'https://mirrors.huaweicloud.com/repository/pypi/simple/',
+        '腾讯云' => 'https://mirrors.cloud.tencent.com/pypi/simple/',
+        '中科大' => 'https://pypi.mirrors.ustc.edu.cn/simple/',
+        '网易' => 'http://mirrors.163.com/pypi/simple/',
+    ];
+
+    /**
+     * 设置 pip 镜像源
+     *
+     * @param string $name
+     * @param string $url
+     * @return bool
+     */
+    public static function addMirror(string $name, string $url): bool
+    {
+        if (!static::$mirrors[$name] ?? null) {
+            static::$mirrors[$name] = $url;
+            return true;
+        }
+        return false;
+    }
+
     /** @inheritdoc */
     protected function configure(): void
     {
@@ -18,43 +49,22 @@ class PipMirrorConfig extends AbstractCommand
             ->setDescription('Set pip mirror');
     }
 
-    protected function execPipCommand(string $pipCommand): string
-    {
-        if ($this->process->executePip($pipCommand, $output) != 0) {
-            throw new \RuntimeException('Failed to execute pip command');
-        }
-        return implode("\n", $output);
-    }
-
     /** @inheritdoc */
     protected function handler(): int
     {
-        $this->consoleIO?->output('Current pip mirror url: ' . $this->execPipCommand('config get global.index-url'));
+
         $this->consoleIO?->output('Select the pip mirror source ...');
-        $options = ['Python 官方', '清华', '阿里云', '华为云', '腾讯云', '中科大', '网易'];
-
-        // 创建选择问题
-        $question = new ChoiceQuestion(
-            '请选择 pip 镜像站:',
-            $options,
-            0
-        );
-
-        $question->setErrorMessage('Invalid option.');
-
-        $helper = new QuestionHelper();
-        $selectedOption = $helper->ask($this->consoleIO?->getInput(), $this->consoleIO?->getOutput(), $question);
-
-        match ($selectedOption) {
-            'Python 官方' => $this->execPipCommand('config set global.index-url https://pypi.org/simple/'),
-            '清华' => $this->execPipCommand('config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple'),
-            '阿里云' => $this->execPipCommand('config set global.index-url http://mirrors.aliyun.com/pypi/simple/'),
-            '华为云' => $this->execPipCommand('config set global.index-url https://mirrors.huaweicloud.com/repository/pypi/simple/'),
-            '腾讯云' => $this->execPipCommand('config set global.index-url https://mirrors.cloud.tencent.com/pypi/simple/'),
-            '中科大' => $this->execPipCommand('config set global.index-url https://pypi.mirrors.ustc.edu.cn/simple/'),
-            '网易' => $this->execPipCommand('config set global.index-url http://mirrors.163.com/pypi/simple/'),
-        };
-
-        return $this->consoleIO?->success('已将 pip 源设置为: ' . $selectedOption);
+        $options = array_keys(static::$mirrors);
+        $selectedOption = $this->consoleIO?->choice('请选择 pip 镜像站:', $options, 0, function (ChoiceQuestion $choice) {
+            $choice->setErrorMessage('Invalid option.');
+        });
+        $indexUrl = static::$mirrors[$selectedOption] ?? null;
+        if (!$indexUrl) {
+            return $this->consoleIO?->error('Invalid option.');
+        }
+        if ($this->process->executePip("config set global.index-url $indexUrl", subOutput: true) !== 0) {
+            return $this->consoleIO?->error('Failed to set pip mirror.');
+        }
+        return $this->consoleIO?->success("已将 pip 源设置为: $selectedOption -> $indexUrl");
     }
 }
