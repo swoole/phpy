b539b1c604d267107def4187cc156b75bd1799ae
chaz6chez <chaz6chez1993@outlook.com>
2025-04-01 18:22:27 +0800

    1. add config:pip-mirror supports pip-mirror.json

--- tools/src/Phpy/Commands/PipMirrorConfig.php
+++ tools/src/Phpy/Commands/PipMirrorConfig.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace PhpyTool\Phpy\Commands;
 
+use PhpyTool\Phpy\Helpers\System;
 use Symfony\Component\Console\Question\ChoiceQuestion;
 
 class PipMirrorConfig extends AbstractCommand
@@ -46,14 +47,42 @@ class PipMirrorConfig extends AbstractCommand
         parent::configure();
         $this
             ->setName('config:pip-mirror')
-            ->setDescription('Set pip mirror');
+            ->setDescription('Set pip mirror')
+            ->setHelp(<<<EOT
+该命令预设部分pip镜像源提供选择变更，并且支持自定义pip镜像源；
+
+<comment>自定义pip镜像源</comment>需在当前项目根目录创建<info>pip-mirror.json</info>，运行时自动加载；
+<info>pip-mirror.json</info>格式样例:
+<info>
+{
+    "Python官方": "https://pypi.org/simple/",
+    "中科大": "https://pypi.mirrors.ustc.edu.cn/simple/"
+}
+</info>
+<comment>自定义pip镜像源</comment>运行时自动加载。
+EOT
+        );
     }
 
     /** @inheritdoc */
     protected function handler(): int
     {
-
         $this->consoleIO?->output('Select the pip mirror source ...');
+        // 加载自定义配置文件
+        $config = System::getcwd(). '/pip-mirror.json';
+        if (file_exists($config)) {
+            try {
+                $config = json_decode(trim(System::getFileContent($config)), true, flags: JSON_THROW_ON_ERROR);
+                if ($config) {
+                    foreach ($config as $name => $url) {
+                        if (filter_var($url, FILTER_VALIDATE_URL)) {
+                            static::addMirror($name, $url);
+                        }
+                    }
+                }
+            } catch (\JsonException) {}
+        }
+        // 选择镜像源
         $options = array_keys(static::$mirrors);
         $selectedOption = $this->consoleIO?->choice('请选择 pip 镜像站:', $options, 0, function (ChoiceQuestion $choice) {
             $choice->setErrorMessage('Invalid option.');
